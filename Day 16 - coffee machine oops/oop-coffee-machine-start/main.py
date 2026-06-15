b6a594c807901f692208edbeb8b0b144e09b7c13
harish-s-1 <harish.work30@gmail.com>
2026-06-15 11:03:58 +0530

    projects based  on oops

---
+++ Day 16 - coffee machine oops/oop-coffee-machine-start/main.py
@@ -0,0 +1,20 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+is_on = True
+
+while is_on:
+    user_input = input(f"What would you like?({menu.get_items()})")
+    if user_input == "quit":
+        is_on = False
+    elif user_input == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(user_input)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)

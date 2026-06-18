983bbca9a32467e0af3164474ce85e2633b0fe00
harish-s-1 <harish.work30@gmail.com>
2026-06-18 11:53:12 +0530

    oops based proejct starts

---
+++ Day 17 - quiz with oops/main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+Question_bank = []
+for i in question_data:
+    question_text = i["text"]
+    question_answer = i["answer"]
+    new_question = Question(question_text, question_answer)
+    Question_bank.append(new_question)
+
+quiz = QuizBrain(Question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("you have completed the quiz")
+print(f"the final score is:{quiz.score}")
---
+++ Day 17 - quiz with oops/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+n_q = Question("dcfdsddas","true")
+print(n_q.question)
+
